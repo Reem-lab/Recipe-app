06024b0f005918fa3a45fef37d33af117314b0be
Reem <janinareem@gmail.com>
2022-05-17 23:19:38 +0200

    Add foods controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,34 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+    @current_user = current_user
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.new(food_params)
+
+    if @food.save
+      redirect_to foods_path, notice: "Your Food is created successfully 💯"
+    else
+      flash[:alert] = "Something went wrong, Try again!"
+      render :new
+    end
+  end
+
+  def destroy
+    @food =  Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
+
+end
