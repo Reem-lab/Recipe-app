22b2f61f79dc7df885233f9697ca4368f9a611b8
Piero Lescano <pierolescano@protonmail.com>
2022-05-18 11:47:25 -0500

    Add total price to public recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 
+  def total_price
+    foods.map { |f| f.price * f.recipe_foods.find_by(recipe_id: id).quantity }.sum
+  end
 end
