6876da1b932ea3c416686e21e0b2a3cc56b476a4
Reem <janinareem@gmail.com>
2022-05-18 21:09:50 +0200

    Add a method to calculate the foods recipes

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,9 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
+
+  def get_total_quantity_recipes
+     recipe_foods.map { | f | f.quantity }.sum
+  end
+
 end
