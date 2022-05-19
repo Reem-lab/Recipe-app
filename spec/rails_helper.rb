ccf5838111a3c3eb10bdfcf4bd2b2d9ad391b0c4
Piero Lescano <pierolescano@protonmail.com>
2022-05-19 11:48:38 -0500

    Add view specs for recipes and shopping list

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,11 +61,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-#   Capybara.register_driver :selenium_chrome do |app|
-#     Capybara::Selenium::Driver.new(app, browser: :chrome)
-#   end
-
-#   Capybara.javascript_driver = :selenium_chrome
-#  Capybara.default_driver = :selenium_chrome
 end
